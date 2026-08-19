f1e99f4093d3c47ec514e32fc36558d297835229
Andre Tardelli <andre.ltardelli@gmail.com>
2026-08-19 19:57:01 +0100

    Imprimindo livro teste na main

--- estrututura-de-dados/structs/registro_de_alunos.c
+++ estrututura-de-dados/structs/registro_de_alunos.c
@@ -13,8 +13,22 @@ Livro *create_livro(char *titulo, unsigned int num_pagina, float preco) {
     Livro *livro = calloc(1, sizeof(Livro));
 
     strcpy(livro->titulo, titulo);
-    livro->num_paginas, num_pagina;
-    livro->preco, preco;
-    
+    livro->num_paginas = num_pagina;
+    livro->preco = preco;
+
     return livro;
+}
+
+void print_livro(const Livro *livro){
+    printf("Titulo: %s\n", livro->titulo);
+    printf("Num. Pagina: %d\n", livro->num_paginas);
+    printf("Preco: R$ %.2f\n", livro->preco); // Float com limite de 2 casas decimais;
+}
+
+int main() {
+    Livro *livro_harry = create_livro("Harry Potter", 200, 25);
+
+    print_livro(livro_harry);
+    
+    return 0;
 }
